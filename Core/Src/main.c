ae43282399d68ebe8652f0ecd4c2908f6c0ff29a
IsaiahW68 <isaiah68wildenberg@gmail.com>
2023-03-10 16:14:51 -0600

    Enable gsense

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -936,6 +936,10 @@ void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim)
     HAL_IncTick();
   }
   /* USER CODE BEGIN Callback 1 */
+  if (htim->Instance == TIM10)
+  {
+    // DAQ_TimerCallback(htim);
+  }
   /* USER CODE END Callback 1 */
 }
 
