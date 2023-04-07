2f7ac42b9b048e611fab9c9139c6fb1f0b971a66
sebasler <43249595+sebasler@users.noreply.github.com>
2023-04-07 00:48:08 -0500

    oopsie forgot this file

--- Core/Src/alerts.c
+++ Core/Src/alerts.c
@@ -39,27 +39,16 @@ static bool overtemperatureFaultPresent(Bms_S* bms)
 
 static bool amsSdcFaultPresent(Bms_S* bms)
 {
-<<<<<<< Updated upstream
-    return bms->amsFault;
-=======
     return HAL_GPIO_ReadPin(AMS_FAULT_SDC_GPIO_Port, AMS_FAULT_SDC_Pin);
->>>>>>> Stashed changes
 }
 
 static bool bspdSdcFaultPresent(Bms_S* bms)
 {
-<<<<<<< Updated upstream
-    return bms->bspdFault;
-=======
     return HAL_GPIO_ReadPin(BSPD_FAULT_SDC_GPIO_Port, BSPD_FAULT_SDC_Pin);
->>>>>>> Stashed changes
 }
 
 static bool imdSdcFaultPresent(Bms_S* bms)
 {
-<<<<<<< Updated upstream
-    return bms->imdFault;
-=======
     return HAL_GPIO_ReadPin(IMD_FAULT_SDC_GPIO_Port, IMD_FAULT_SDC_Pin);
 }
 
@@ -67,7 +56,6 @@ static bool currentSensorErrorPresent(Bms_S* bms)
 {
     // TODO: Implement current sense error check
     return false;
->>>>>>> Stashed changes
 }
 
 
